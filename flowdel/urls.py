859e8aced368f1f5079b036ac942c2f2738adee7
Rasoulsa <saeidirasoul@gmail.com>
2021-05-07 23:34:02 +0430

    Add debug toolbar

--- flowdel/urls.py
+++ flowdel/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -5,4 +6,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace="users")),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
