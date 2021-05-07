18ec9ad6125cd128165f608181fba5ebd59b24f9
Rasoulsa <saeidirasoul@gmail.com>
2021-05-07 23:55:38 +0430

    Edited toolbar debug

--- flowdel/urls.py
+++ flowdel/urls.py
@@ -1,10 +1,14 @@
 import debug_toolbar
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace="users")),
-    path('__debug__/', include(debug_toolbar.urls)),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)), ]
